35435e0bb2686f45601d3dc51f4c94022963aa9e
Chris Phillips <you@example.com>
2019-05-17 08:02:11 -0400

    Lab 17

---
+++ AmazonPrime.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17
+{
+    class AmazonPrime
+    {
+        public static int GetPrime(int num)//sequence number
+        {
+            bool isPrime;
+            int prime = 2;
+            int count = 0;
+            List<int> factors = new List<int>();
+            List<int> primes = new List<int>();
+
+            while (count < num)
+            {
+                factors.Clear();
+                isPrime = true;
+
+                for (int i = prime - 1; i > 0; i--)// i= 1
+                {
+                    if (prime % i == 0)
+                    {
+                        factors.Add(i);//list has : 1
+                    }
+                }
+                if (factors.Count > 1)
+                {
+                    isPrime = false;
+
+                }
+
+                if (isPrime)
+                {
+                    count++;
+                    prime++;
+                }
+                else
+                {
+                    prime++;
+                }
+            }
+
+            return prime - 1;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Prime Finder! \nIn A List of Primes, Which Prime Number are you looking for?");
+            int Ask = int.Parse(Console.ReadLine());
+            Console.WriteLine(AmazonPrime.Optimus(Ask));
+        }
+    }
+}
